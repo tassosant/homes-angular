6ac69c80b243aa621d5f11cea6852e433d7c5c8b
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2024-05-26 21:25:52 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  template:`
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/home.svg" alt="logo" aria-hidden="true" width="100">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'homes';
 }
